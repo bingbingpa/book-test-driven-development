d029d2f87e1b12847ba261654bc283afc7bffdee
shpark <bingbingpa@gmail.com>
2020-11-04 00:52:04 +0900

    init

---
+++ src/main/java/com/bingbingpa/BookTestDrivenDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.bingbingpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTestDrivenDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTestDrivenDevelopmentApplication.class, args);
+    }
+
+}
