583d8c037616713b6d19bf640a0fef816eae3dc6
shpark <bingbingpa@gmail.com>
2020-11-18 01:03:23 +0900

    암호 판별 로직 리팩토링

--- src/test/java/com/bingbingpa/ch02/PasswordStrengthMeter.java
+++ src/test/java/com/bingbingpa/ch02/PasswordStrengthMeter.java
@@ -3,24 +3,12 @@ package com.bingbingpa.ch02;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
-
-        if (lengthEnough && !containsNum && !containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!lengthEnough && containsNum && !containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!lengthEnough && !containsNum && containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!lengthEnough) {
-            return PasswordStrength.NORMAL;
-        }
-        if (!containsNum) return PasswordStrength.NORMAL;
-        if(!containsUpp) return PasswordStrength.NORMAL;
+        int meetCounts = 0;
+        if (s.length() >= 8) meetCounts++;
+        if (meetsContainingNumberCriteria(s)) meetCounts++;
+        if (meetsContainingUppercaseCriteria(s)) meetCounts++;
+        if (meetCounts == 1) return PasswordStrength.WEAK;
+        if (meetCounts == 2) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
     }
 
