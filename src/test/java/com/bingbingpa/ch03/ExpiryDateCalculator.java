8680b3b8a878c43222f1f5616fed49a78ae3017f
shpark <bingbingpa@gmail.com>
2020-11-20 00:35:46 +0900

    test : 가장 쉬운 테스트 케이스 작성

---
+++ src/test/java/com/bingbingpa/ch03/ExpiryDateCalculator.java
@@ -0,0 +1,10 @@
+package com.bingbingpa.ch03;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019, 4, 1);
+    }
+}
