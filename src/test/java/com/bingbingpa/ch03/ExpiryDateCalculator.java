43454f3dcfb5b489c44ca70a21e0ac4a83f45a06
shpark <bingbingpa@gmail.com>
2020-11-24 01:11:56 +0900

    feat : 지불 금액에 따른 만료일 계산

--- src/test/java/com/bingbingpa/ch03/ExpiryDateCalculator.java
+++ src/test/java/com/bingbingpa/ch03/ExpiryDateCalculator.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(PayData payData) {
-        int addMonths = 1;
+        int addMonths = payData.getPlayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
